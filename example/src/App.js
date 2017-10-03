f037fdff328a180e6305146958b2419ea24cece7
alejo <alejodaraio@gmail.com>
2017-10-03 16:09:54 -0300

    fix path

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
 import './App.css';
-import * as Genoa from "react-genoa-player";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Genoa playerId={1234} videoId={1123} host="https://domain.com"/>
+
             </div>
         );
     }
