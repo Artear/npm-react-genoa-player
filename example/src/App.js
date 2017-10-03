1ab5d5cba73a6c4635e9c1144c2cd539eac5cf0c
alejo <alejodaraio@gmail.com>
2017-10-03 15:55:01 -0300

    add .npmignore

--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
-import Genoa from "react-genoa-player";
+import * as Genoa from "react-genoa-player";
 
 class App extends Component {
     render() {
